3b94108854d1eeded4e1717bfdd5a45d8deac515
Yam Ydo <yam.fontalba@gmail.com>
2023-03-26 18:16:45 -0300

    feat: added modal for post comments
    - Modal for post comments
    - New method for get post comments
    - Responsive modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         <div class="post-list">
-            <div v-for="(post, index) in postList" :key="index" class="post">
+            <div v-for="(post, index) in postList" :key="index" class="post" @click="getPostComments(post.id, post.title)">
                 <h2>{{ post.title }}</h2>
                 <p>{{ post.body }}</p>
             </div>
         </div>
+
+        <b-modal v-model="modalShow" size="xl" title="Comentários do post">
+            <template #modal-header>
+                <h2 class="comment-post-title"> {{ postTitle }} </h2>
+            </template>
+
+            <div class="post-list-comment">
+                <div v-for="(comment, index) in postListComments" :key="index" class="comment">
+                    <div class="comment-post-header">
+                        <div class="comment-post-name"> {{ comment.name }} </div>
+                        <div> {{ comment.email }}</div>
+                    </div>
+                    <div class="comment-post-body"> {{ comment.body }} </div>
+                </div>
+            </div>
+        </b-modal>
     </div>
 </template>
   
@@ -18,6 +34,9 @@ export default {
     data() {
         return {
             postList: [],
+            modalShow: false,
+            postListComments: [],
+            postTitle: null,
         };
     },
 
@@ -29,6 +48,13 @@ export default {
         async getPostList() {
             const response = await api.get("/posts");
             this.postList = response.data;
+            this.addPosts()
+        },
+        async getPostComments(id, title) {
+            const response = await api.get(`/posts/${id}/comments`);
+            this.postListComments = response.data;
+            this.postTitle = title;
+            this.modalShow = !this.modalShow;
         },
     },
 };
@@ -70,6 +96,41 @@ export default {
     margin: 0;
     text-align: justify;
     height: 150px;
+}
+
+.post-list-comment {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 30px;
+    padding: 15px;
+    box-sizing: border-box;
+}
+
+.comment {
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    padding: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.comment-post-header {
+    display: flex;
+    justify-content: flex-start;
+    padding: 5px 0px;
+    font-weight: 600;
+}
+
+.comment-post-name {
+    padding: 0px 15px 0px 0px;
+    color: #1755ca;
+}
+
+.comment-post-body {
+    text-align: justify;
+}
+
+.comment-post-title {
+    padding-left: 20px;
 }
 
 @media only screen and (min-width: 320px) {
